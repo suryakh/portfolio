8b0706ffc8cb37629a9a801f82cd210d171ea831
Surya Khandavilli <Khandavillisurya@gmail.com>
2020-03-05 15:07:38 +0530

    P

--- src/Component.js/Projects.jsx
+++ src/Component.js/Projects.jsx
@@ -23,7 +23,7 @@ export default class Projects extends Component {
         return (
             <div>
                 <h1 className="header" data-aos="fade-up" data-aos-duration="1000">Projects</h1>
-                <div className="container">
+                <div className="container projectdiv">
                     <div className="row">
                         <div className="col-12">
                             <div className="col-12" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="250">
@@ -31,7 +31,7 @@ export default class Projects extends Component {
                                     <img className="img-fluid card_img" style={{ "height": "auto" }} src={Google} />
                                     <div className="card_text col-12">
                                         <div className="col-12" >
-                                            <a href="https://googletimer.netlify.com" target="blank"><h3>GoogleTimer & GoogleStopWatch</h3></a>
+                                            <a className="stretched-link" href="https://googletimer.netlify.com" target="blank"><h3>GoogleTimer & GoogleStopWatch</h3></a>
                                             <br />
                                             <p> My first React application</p>
                                             <div className="col-12 d-flex justify-content-center">
@@ -53,7 +53,7 @@ export default class Projects extends Component {
                                     <img className="img-fluid card_img" style={{ "height": "auto" }} src={Todo} />
                                     <div className="card_text col-12">
                                         <div className="col-12">
-                                            <a href="https://checklist-todo.netlify.com" target="blank"><h4>TODO Application</h4></a>
+                                            <a className="stretched-link" href="https://checklist-todo.netlify.com" target="blank"><h4>TODO Application</h4></a>
                                             <p> My React+Redux application</p>
                                             <div className="col-12 d-flex justify-content-center">
                                                 <div className="row">
@@ -74,16 +74,16 @@ export default class Projects extends Component {
                                     <img className="img-fluid card_img" style={{ "height": "auto" }} src={OMDB} />
                                     <div className="card_text col-12">
                                         <div className="col-12">
-                                            <a href="https://omdbproject.netlify.com" target="blank"><h3>Open Movie Data Base</h3></a>
+                                            <a className="stretched-link" href="https://omdbproject.netlify.com" target="blank"><h3>Open Movie Data Base</h3></a>
                                             <p> My first API Request application</p>
                                             <div className="col-12 d-flex justify-content-center">
                                                 <div className="row">
                                                     {/* <div className="col-1"></div> */}
-                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }}  src={htmlsvg} /></div>
-                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }}  src={csssvg} /></div>
-                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }}  src={jssvg} /></div>
-                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }}  src={jquerysvg} /></div>
-                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }}  src={boostrapsvg} /></div>
+                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }} src={htmlsvg} /></div>
+                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }} src={csssvg} /></div>
+                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }} src={jssvg} /></div>
+                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }} src={jquerysvg} /></div>
+                                                    <div className="col-2"><img style={{ height: "30px", width: "30px" }} src={boostrapsvg} /></div>
                                                     {/* <div className="col-1"></div> */}
                                                 </div>
                                             </div>
@@ -98,7 +98,7 @@ export default class Projects extends Component {
                                     <img className="img-fluid card_img" style={{ "height": "auto" }} src={Bill} />
                                     <div className="card_text col-12">
                                         <div className="col-12">
-                                            <a href="https://billgenerator.netlify.com" target="blank"><h3>Bill Generator</h3></a>
+                                            <a className="stretched-link" href="https://billgenerator.netlify.com" target="blank"><h3>Bill Generator</h3></a>
                                             <p> My JavaScript application</p>
                                             <div className="col-12 d-flex justify-content-center">
                                                 <div className="row">
@@ -118,7 +118,7 @@ export default class Projects extends Component {
                                     <img className="img-fluid card_img" style={{ "height": "auto" }} src={Marksrecorder} />
                                     <div className="card_text col-12">
                                         <div className="col-12">
-                                            <a href="https://marksrecorder.netlify.com" target="blank"><h3>Marks Recorder</h3></a>
+                                            <a className="stretched-link" href="https://marksrecorder.netlify.com" target="blank"><h3>Marks Recorder</h3></a>
                                             <p> My Jquery application</p>
                                             <div className="col-12 d-flex justify-content-center">
                                                 <div className="row">
@@ -138,7 +138,7 @@ export default class Projects extends Component {
                                     <img className="img-fluid card_img" style={{ "height": "auto" }} src={TaskManager} />
                                     <div className="card_text col-12">
                                         <div className="col-12" >
-                                            <a href="https://github.com/suryakh/Taskmanager/tree/master/task_list" target="blank"><h3>Task Manager</h3></a>
+                                            <a className="stretched-link" href="https://github.com/suryakh/Taskmanager/tree/master/task_list" target="blank"><h3>Task Manager</h3></a>
                                             <br />
                                             <p> My End to End backend+ front end application</p>
                                             <div className="col-12 d-flex justify-content-center">
